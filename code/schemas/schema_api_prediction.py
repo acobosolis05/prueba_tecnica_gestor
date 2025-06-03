bd340d0bff183a5e6bc96fb989e11e6d3c76e8cb
airam <airamcobosolis@gmail.com>
2025-06-02 23:02:05 -0500

    folder with schemas file

--- code/schemas/schema_api_prediction.py
+++ code/schemas/schema_api_prediction.py
@@ -1,1 +1,20 @@
-#
+from pydantic import BaseModel
+from typing import Optional
+
+class PredictionSchema(BaseModel):
+    autoID: Optional [str] = ''
+    SeniorCity: Optional[int] = None
+    Partner: str
+    Dependents: str
+    Service1: str
+    Service2: str
+    Security: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    Charges: float
+    Demand: float
+    
